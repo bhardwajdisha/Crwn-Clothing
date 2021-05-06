9ce7e1932e802c9b94be2d7032665b9e10eb0fa4
Disha Bhardwaj <disha2.bhardwaj@gmail.com>
2021-05-06 14:58:37 +0530

    Updated menu-item to navigate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import {Homepage} from './pages/homepage'
+import {Route } from 'react-router-dom'
 
+const Hats= ()=>{
+  return(
+    <div>
+      <h1 style={{textAlign: 'center'}}>Hats page</h1>
+      
+    </div>
+  )
+}
 function App() {
   return (
     <div>
-        <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/hats" component={Hats} />
     </div>
   );
 }
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,45 +1,10 @@
 import React from 'react'
 import './homepage.scss'
+import Directory from '../components/directory-menu/directory-menu'
 export const Homepage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Hats</h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Hats</h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Hats</h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Hats</h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Hats</h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-            </div>
+            < Directory />
         </div>
     )
 }
