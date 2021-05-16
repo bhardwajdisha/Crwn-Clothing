1eb6454f6f0522a071526357ae5933389a53cbfb
Disha Bhardwaj <disha2.bhardwaj@gmail.com>
2021-05-16 13:01:55 +0530

    Created new directoryReducer and shifted directory data to redux store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-// import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react'
+
 import { Provider} from 'react-redux'
-import store from './redux/store'
+import {store , persist} from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persist}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
     </Provider>,
   document.getElementById('root')
 );
+
+
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import reportWebVitals from './reportWebVitals';
